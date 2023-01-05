3bc7cd682e03c4bd3add28a35928582a675862f2
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2023-01-05 20:40:28 +0200

    auth component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,13 @@
 import type { Component } from 'solid-js';
-import { Router } from '@solidjs/router'
+import { Router, Route } from '@solidjs/router'
 
 import styles from './App.module.css';
+import Auth from './components/Auth/Auth';
 
 const App: Component = () => {
   return (
     <Router>
+      <Route path="/users" component={Auth} />
       <div class={styles.App}>
       </div>
     </Router>
